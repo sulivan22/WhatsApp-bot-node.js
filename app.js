ccc4247ee9642e356f9bdfb7df578d21ed83686f
SEAD <sulivan22@gmail.com>
2022-03-30 14:20:12 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,161 @@
-const { Client } = require('whatsapp-web.js');
+/**
+ * ⚡⚡⚡ DECLARAMOS LAS LIBRERIAS y CONSTANTES A USAR! ⚡⚡⚡
+ */
+
+
+
+require('dotenv').config()
+    //const fs = require('fs');
+const express = require('express');
+const cors = require('cors')
 const qrcode = require('qrcode-terminal');
-const client = new Client();
+const { Client } = require('whatsapp-web.js');
+const { middlewareClient } = require('./middleware/client')
+const { generateImage, cleanNumber } = require('./controllers/handle')
+const { connectionReady } = require('./controllers/connection')
+    //const { saveMedia } = require('./controllers/save')
+const { getMessages, responseMessages } = require('./controllers/flows')
+const { sendMedia, sendMessage, lastTrigger, sendMessageButton, readChat } = require('./controllers/send')
+const app = express();
+app.use(cors())
+app.use(express.json())
 
-client.on('qr', qr => {
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server, {
+    cors: {
+        origins: ['http://localhost:4200']
+    }
+})
+
+let socketEvents = { sendQR: () => {}, sendStatus: () => {} };
+
+io.on('connection', (socket) => {
+    const CHANNEL = 'main-channel';
+    socket.join(CHANNEL);
+    socketEvents = require('./controllers/socket')(socket)
+    console.log('Se conecto')
+})
+
+app.use('/', require('./routes/web'))
+
+const port = process.env.PORT || 3000
+
+var client;
+
+/**
+ * Escuchamos cuando entre un mensaje
+ */
+const listenMessage = () => client.on('message', async msg => {
+    const { from, body, hasMedia } = msg;
+    // Este bug lo reporto Lucas Aldeco Brescia para evitar que se publiquen estados
+    if (from === 'status@broadcast') {
+        return
+    }
+    message = body.toLowerCase();
+    console.log('BODY', message)
+    const number = cleanNumber(from)
+    await readChat(number, message)
+
+    /**
+     * Guardamos el archivo multimedia que envia
+     */
+    // if (process.env.SAVE_MEDIA && hasMedia) {
+    //     const media = await msg.downloadMedia();
+    //     saveMedia(media);
+    // }
+
+
+    /**
+     * Ver si viene de un paso anterior
+     * Aqui podemos ir agregando más pasos
+     * a tu gusto!
+     */
+
+    const lastStep = await lastTrigger(from) || null;
+    console.log({ lastStep })
+    if (lastStep) {
+        const response = await responseMessages(lastStep)
+        await sendMessage(client, from, response.replyMessage);
+    }
+
+    /**
+     * Respondemos al primero paso si encuentra palabras clave
+     */
+    const step = await getMessages(message);
+    console.log({ step })
+
+    if (step) {
+        const response = await responseMessages(step);
+
+        /**
+         * Si quieres enviar botones
+         */
+
+        await sendMessage(client, from, response.replyMessage, response.trigger);
+        if (response.hasOwnProperty('actions')) {
+            const { actions } = response;
+            await sendMessageButton(client, from, null, actions);
+            return
+        }
+
+        if (!response.delay && response.media) {
+            sendMedia(client, from, response.media);
+        }
+        if (response.delay && response.media) {
+            setTimeout(() => {
+                sendMedia(client, from, response.media);
+            }, response.delay)
+        }
+        return
+    }
+
+    //Si quieres tener un mensaje por defecto
+    if (process.env.DEFAULT_MESSAGE === 'true') {
+        const response = await responseMessages('DEFAULT')
+        await sendMessage(client, from, response.replyMessage, response.trigger);
+
+        /**
+         * Si quieres enviar botones
+         */
+        if (response.hasOwnProperty('actions')) {
+            const { actions } = response;
+            await sendMessageButton(client, from, null, actions);
+        }
+        return
+    }
+});
+
+/**
+ * Generamos un QRCODE para iniciar sesion
+ */
+
+client = new Client();
+
+client.on('qr', qr => generateImage(qr, () => {
     qrcode.generate(qr, { small: true });
+    console.log(`Ver QR http://localhost:${port}/qr`)
+    socketEvents.sendQR(qr)
+}))
+
+client.on('ready', (a) => {
+    connectionReady()
+    listenMessage()
+    loadRoutes(client);
+    socketEvents.sendStatus(client)
 });
-
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
-
-client.on('message', message => {
-    console.log(message.body);
-    if (message.body === 'test') {
-        client.sendMessage(message.from, 'https://cdn.shopify.com/s/files/1/1061/1924/products/Flushed_Emoji_Icon_5e6ce936-4add-472b-96ba-9082998adcf7.png?v=1485573448');
-    }
-    if (message.body === 'hola') {
-        client.sendMessage(message.from, 'https://Instagram.com/doc.maltes');
-    }
-});
-
-
-
 
 client.initialize();
+
+/**
+ * Cargamos rutas de express
+ */
+
+const loadRoutes = (client) => {
+    app.use('/api/', middlewareClient(client), require('./routes/api'))
+}
+
+
+server.listen(port, () => {
+    console.log(`El server esta listo por el puerto ${port}`);
+})
---
+++ controllers/send.js
@@ -0,0 +1,140 @@
+const ExcelJS = require('exceljs');
+const moment = require('moment');
+const fs = require('fs');
+const { MessageMedia, Buttons } = require('whatsapp-web.js');
+const { cleanNumber } = require('./handle')
+const DELAY_TIME = 170; //ms
+const DIR_MEDIA = `${__dirname}/../mediaSend`;
+
+/**
+ * Enviamos archivos multimedia a nuestro cliente
+ * @param {*} number 
+ * @param {*} fileName 
+ */
+
+const sendMedia = (client, number, fileName) => {
+    number = cleanNumber(number)
+    const file = `${DIR_MEDIA}/${fileName}`;
+    if (fs.existsSync(file)) {
+        const media = MessageMedia.fromFilePath(file);
+        client.sendMessage(number, media, { sendAudioAsVoice: true });
+    }
+}
+
+/**
+ * Enviamos archivos como notas de voz
+ * @param {*} number 
+ * @param {*} fileName 
+ */
+
+const sendMediaVoiceNote = (client, number, fileName) => {
+    number = cleanNumber(number)
+    const file = `${DIR_MEDIA}/${fileName}`;
+    if (fs.existsSync(file)) {
+        const media = MessageMedia.fromFilePath(file);
+        client.sendMessage(number, media, { sendAudioAsVoice: true });
+    }
+}
+
+/**
+ * Enviamos un mensaje simple (texto) a nuestro cliente
+ * @param {*} number 
+ */
+const sendMessage = async(client, number = null, text = null, trigger = null) => {
+    setTimeout(async() => {
+        number = cleanNumber(number)
+        const message = text
+        client.sendMessage(number, message);
+        await readChat(number, message, trigger)
+        console.log(`⚡⚡⚡ Enviando mensajes....`);
+    }, DELAY_TIME)
+}
+
+/**
+ * Enviamos un mensaje con buttons a nuestro cliente
+ * @param {*} number 
+ */
+const sendMessageButton = async(client, number = null, text = null, actionButtons) => {
+    number = cleanNumber(number)
+    const { title = null, message = null, footer = null, buttons = [] } = actionButtons;
+    let button = new Buttons(message, [...buttons], title, footer);
+    client.sendMessage(number, button);
+
+    console.log(`⚡⚡⚡ Enviando mensajes....`);
+}
+
+
+/**
+ * Opte
+ */
+const lastTrigger = (number) => new Promise((resolve, reject) => {
+    number = cleanNumber(number)
+    const pathExcel = `${__dirname}/../chats/${number}.xlsx`;
+    const workbook = new ExcelJS.Workbook();
+    if (fs.existsSync(pathExcel)) {
+        workbook.xlsx.readFile(pathExcel)
+            .then(() => {
+                const worksheet = workbook.getWorksheet(1);
+                const lastRow = worksheet.lastRow;
+                const getRowPrevStep = worksheet.getRow(lastRow.number);
+                const lastStep = getRowPrevStep.getCell('C').value;
+                resolve(lastStep)
+            });
+    } else {
+        resolve(null)
+    }
+})
+
+/**
+ * Guardar historial de conversacion
+ * @param {*} number 
+ * @param {*} message 
+ */
+const readChat = async(number, message, trigger = null) => {
+    setTimeout(() => {
+        number = cleanNumber(number)
+        const pathExcel = `${__dirname}/../chats/${number}.xlsx`;
+        const workbook = new ExcelJS.Workbook();
+        const today = moment().format('DD-MM-YYYY hh:mm')
+
+        if (fs.existsSync(pathExcel)) {
+            /**
+             * Si existe el archivo de conversacion lo actualizamos
+             */
+            const workbook = new ExcelJS.Workbook();
+            workbook.xlsx.readFile(pathExcel)
+                .then(() => {
+                    const worksheet = workbook.getWorksheet(1);
+                    const lastRow = worksheet.lastRow;
+                    var getRowInsert = worksheet.getRow(++(lastRow.number));
+                    getRowInsert.getCell('A').value = today;
+                    getRowInsert.getCell('B').value = message;
+                    getRowInsert.getCell('C').value = trigger;
+                    getRowInsert.commit();
+                    workbook.xlsx.writeFile(pathExcel);
+                });
+
+        } else {
+            /**
+             * NO existe el archivo de conversacion lo creamos
+             */
+            const worksheet = workbook.addWorksheet('Chats');
+            worksheet.columns = [
+                { header: 'Fecha', key: 'number_customer' },
+                { header: 'Mensajes', key: 'message' },
+                { header: 'Disparador', key: 'trigger' }
+            ];
+            worksheet.addRow([today, message, trigger]);
+            workbook.xlsx.writeFile(pathExcel)
+                .then(() => {
+
+                    console.log("saved");
+                })
+                .catch((err) => {
+                    console.log("err", err);
+                });
+        }
+    }, 150)
+}
+
+module.exports = { sendMessage, sendMedia, lastTrigger, sendMessageButton, readChat, sendMediaVoiceNote }
